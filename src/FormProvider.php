6c6293566bf20880e6d5b713eab76cf6372ee60a
Frank Heider <heider669@googlemail.com>
2018-06-15 16:54:44 +0200

    start project

---
+++ src/FormProvider.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Brnbio\LaravelForm;
+
+use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\ServiceProvider;
+
+class FormServiceProvider extends ServiceProvider
+{
+    /**
+     * @return void
+     */
+    public function boot()
+    {
+
+    }
+
+    /**
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
---
+++ src/helpers.php
@@ -0,0 +1,15 @@
+<?php
+
+if (! function_exists('form')) {
+    /**
+     * Get the available container instance.
+     *
+     * @param  string  $abstract
+     * @param  array   $parameters
+     * @return mixed|\Illuminate\Foundation\Application
+     */
+    function form()
+    {
+        return \Brnbio\LaravelForm\FormHelper::getInstance();
+    }
+}
