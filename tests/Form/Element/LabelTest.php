08919e45934ec0e360a917489536bd2aeab1b8f0
Frank Heider <heider669@googlemail.com>
2018-10-08 17:28:23 +0200

    unit test for label element

--- tests/Form/Element/LabelTest.php
+++ tests/Form/Element/LabelTest.php
@@ -10,7 +10,8 @@
 
 declare(strict_types=1);
 
-
+use Brnbio\LaravelForm\Form\Helper;
+use Brnbio\LaravelForm\Form\Element\Label;
 use Tests\TestCase;
 
 /**
@@ -20,9 +21,46 @@ use Tests\TestCase;
  */
 class LabelTest extends TestCase
 {
-    public function testElementValid()
+    /**
+     * Simple class without any attributes
+     *
+     * @return void
+     */
+    public function testSimpleElement()
     {
-        $element = new \Brnbio\LaravelForm\Form\Element\Label('Test');
-        $this->assertEquals('<label>Test</label>', $element->render());
+        $element = new Label('Test');
+        $this->assertEquals('<label>Test</label>', (string)$element->render());
+    }
+
+    /**
+     * "for" is an valid attribute > should be parsed
+     *
+     * @return void
+     */
+    public function testElementWithValidAttributes()
+    {
+        $element = new Label('Test', ['for' => 'test']);
+        $this->assertEquals('<label for="test">Test</label>', (string)$element->render());
+    }
+
+    /**
+     * "name" is an invalid attribute for a label element > should be deleted
+     *
+     * @return void
+     */
+    public function testElementWithInvalidAttributes()
+    {
+        $element = new Label('Test', ['name' => 'test']);
+        $this->assertEquals('<label>Test</label>', (string)$element->render());
+    }
+
+    /**
+     * Test helper method with a simple call without attributes
+     *
+     * @return void
+     */
+    public function testHelper()
+    {
+        $this->assertEquals('<label>Test</label>', (string)Helper::getInstance()->label('Test'));
     }
 }
