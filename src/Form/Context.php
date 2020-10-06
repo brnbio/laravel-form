63745caf3a80a4b8c0ebd58a6db3c1233c4c5442
Frank Heider <heider669@gmail.com>
2020-10-06 17:16:56 +0200

    add context detection for sqlite. resolves #9

--- src/Form/Context.php
+++ src/Form/Context.php
@@ -12,11 +12,12 @@ declare(strict_types=1);
 
 namespace Brnbio\LaravelForm\Form;
 
+use Illuminate\Database\Connection;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\DB;
 
 /**
  * Class Context
+ *
  * @package Brnbio\LaravelForm\Form
  */
 class Context
@@ -33,6 +34,7 @@ class Context
 
     /**
      * Context constructor.
+     *
      * @param Model $entity
      */
     public function __construct(Model $entity)
@@ -47,7 +49,7 @@ class Context
      */
     public function getMetadata(string $fieldName): array
     {
-        if (! empty($this->metadata[$fieldName])) {
+        if ( !empty($this->metadata[$fieldName])) {
             return $this->metadata[$fieldName];
         }
 
@@ -68,18 +70,58 @@ class Context
      */
     private function initMetadata(string $tableName): array
     {
-        $metadata = [];
         $connection = $this->getEntity()->getConnection();
-        $fields = $connection->select('SHOW FIELDS FROM ' . $tableName);
 
+        if ($connection->getDriverName() === 'mysql') {
+            return $this->getMysqlMetadata($connection, $tableName);
+        }
+
+        if ($connection->getDriverName() === 'sqlite') {
+            return $this->getSqliteMetadata($connection, $tableName);
+        }
+
+        return [];
+    }
+
+    /**
+     * @param Connection $connection
+     * @param string $tableName
+     * @return array
+     */
+    private function getMysqlMetadata(Connection $connection, string $tableName): array
+    {
+        $metadata = [];
+        $fields = $connection->select('SHOW FIELDS FROM ' . $tableName);
         foreach ($fields as $field) {
             preg_match('/([a-z]+)\(*(\d*)\)*\s?([a-z\s]*)/', $field->Type, $fieldType);
             $metadata[$field->Field] = [
-                'name' => $field->Field,
-                'type' => $fieldType[1],
-                'required' => $field->Null === 'NO' && $field->Default === null,
-                'default' => $field->Default,
-                'maxlength' => ! empty($fieldType[2]) ? (int)$fieldType[2] : null,
+                'name'      => $field->Field,
+                'type'      => $fieldType[1],
+                'required'  => $field->Null === 'NO' && $field->Default === null,
+                'default'   => $field->Default,
+                'maxlength' => !empty($fieldType[2]) ? (int) $fieldType[2] : null,
+            ];
+        }
+
+        return $metadata;
+    }
+
+    /**
+     * @param Connection $connection
+     * @param string $tableName
+     * @return array
+     */
+    private function getSqliteMetadata(Connection $connection, string $tableName): array
+    {
+        $metadata = [];
+        $fields = $connection->select('PRAGMA table_info(' . $tableName . ')');
+        foreach ($fields as $field) {
+            $metadata[$field->name] = [
+                'name'      => $field->name,
+                'type'      => $field->type,
+                'required'  => (boolean) $field->notnull,
+                'default'   => $field->dflt_value,
+                'maxlength' => null,
             ];
         }
 
